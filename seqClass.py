e709c795964d1918cacf613f4368aea4724f00b5
Brianna Malaya Regan <regan.briannamalaya@gmail.com>
2026-06-15 11:34:30 -0400

    Fix sequence classification and add motif search

--- seqClass.py
+++ seqClass.py
@@ -5,19 +5,37 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser(description='Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type=str, required=True, help="Input sequence")
+parser.add_argument("-m", "--motif", type=str, required=False, help="Motif")
 
 if len(sys.argv) == 1:
     parser.print_help()
     sys.exit(1)
 
 args = parser.parse_args()
+args.seq = args.seq.upper()
 
 if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
+
+    if re.search('T', args.seq) and re.search('U', args.seq):
+        print('The sequence is not DNA nor RNA')
+
+    elif re.search('T', args.seq):
         print('The sequence is DNA')
+
     elif re.search('U', args.seq):
         print('The sequence is RNA')
+
     else:
         print('The sequence can be DNA or RNA')
+
 else:
     print('The sequence is not DNA nor RNA')
+
+if args.motif:
+    args.motif = args.motif.upper()
+    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end='')
+
+    if re.search(args.motif, args.seq):
+        print("FOUND")
+    else:
+        print("NOT FOUND")
